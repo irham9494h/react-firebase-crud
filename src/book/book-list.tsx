516710d736acf2f327f5eb90f7f4c89485f9709f
Ahmad Irham <irham9494h@gmail.com>
2022-09-09 00:39:11 +0800

    Createing book list table

---
+++ src/book/book-list.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const BookList = () => {
+  return (
+    <>
+      <div className="bg-yellow-300 border-yellow-500 border-2 shadow px-4 py-2 ">
+        List of Books
+      </div>
+      <div className="text-sm">
+        <table className="w-full">
+          <thead>
+            <tr className="text-md font-semibold tracking-wide text-left text-gray-900 bg-gray-100 uppercase border-b border-gray-600">
+              <th className="px-4 py-3">Book Title</th>
+              <th className="px-4 py-3">Author</th>
+              <th className="px-4 py-3">Pages</th>
+              <th className="px-4 py-3">Year</th>
+            </tr>
+          </thead>
+          <tbody className="bg-white">
+            <tr className="text-gray-700">
+              <td className="px-4 py-2 border">
+                Crud using react and firebase
+              </td>
+              <td className="px-4 py-2 border">John Doe</td>
+              <td className="px-4 py-2 border">250</td>
+              <td className="px-4 py-2 border">2015</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
+
+export default BookList;
--- src/book/index.tsx
+++ src/book/index.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from "react";
+import BookList from "./book-list";
 import BookForm from "./form";
 
 const Book: FC = () => {
@@ -7,10 +8,8 @@ const Book: FC = () => {
       <div className="w-2/6 space-y-2">
         <BookForm />
       </div>
-      <div className="w-4/6">
-        <div className="bg-yellow-300 border-yellow-500 border-2 shadow px-4 py-2 ">
-          List of Books
-        </div>
+      <div className="w-4/6 space-y-2">
+        <BookList />
       </div>
     </div>
   );
