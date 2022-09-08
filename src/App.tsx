285361b81cfa292ebfc48056c8aa116aae61e9e4
Ahmad Irham <irham9494h@gmail.com>
2022-09-08 23:49:36 +0800

    Creating the header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="antialiased">
+      <div className="bg-yellow-400 text-slate-900 h-14 flex justify-between items-center px-20 text-base">
+        <div className="h-full px-4 flex justify-center items-center font-semibold">
+          <h5>REACT JS + FIREBASE</h5>
+        </div>
+        <div className="bg-white rounded px-6 py-1">
+          <h5>BASIC CRUD</h5>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
