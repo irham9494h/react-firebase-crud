7e025d8da0a745047829e631f1ec78fbeaa2b0ad
Ahmad Irham <irham9494h@gmail.com>
2022-09-24 22:53:29 +0800

    Adding realtime data update listener

--- src/book/book-list.tsx
+++ src/book/book-list.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useEffect, useState } from "react";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, onSnapshot } from "firebase/firestore";
 import { db } from "../firebase.config";
 
 interface IBook {
@@ -16,7 +16,6 @@ const BookList: FC = () => {
 
   const getBooks = async () => {
     const querySnapshot = await getDocs(collection(db, "books"));
-
     var book: IBook[] = querySnapshot.docs.map((doc) => ({
       title: doc.data().title,
       author: doc.data().author,
@@ -25,12 +24,35 @@ const BookList: FC = () => {
       synopsis: doc.data().synopsis,
       id: doc.id,
     }));
-
     setBooks(book);
   };
 
+  const handleEditBook = () => {};
+
   useEffect(() => {
-    getBooks();
+    const booksCollectionRef = collection(db, "books");
+    const unsubscribe = onSnapshot(
+      booksCollectionRef,
+      (snapshot) => {
+        console.log("you called me!");
+        var book: IBook[] = snapshot.docs.map((doc) => ({
+          title: doc.data().title,
+          author: doc.data().author,
+          numberOfPages: doc.data().number_of_pages,
+          publishedYear: doc.data().published_year,
+          synopsis: doc.data().synopsis,
+          id: doc.id,
+        }));
+        setBooks(book);
+      },
+      (error) => {
+        console.log(error.message);
+      }
+    );
+
+    return () => {
+      unsubscribe();
+    };
   }, []);
 
   return (
@@ -46,6 +68,7 @@ const BookList: FC = () => {
               <th className="px-4 py-3">Author</th>
               <th className="px-4 py-3">Pages</th>
               <th className="px-4 py-3">Year</th>
+              <th className="px-4 py-3">action</th>
             </tr>
           </thead>
           <tbody className="bg-white">
@@ -53,10 +76,15 @@ const BookList: FC = () => {
               books.map((data, index) => {
                 return (
                   <tr className="text-gray-700" key={index}>
-                    <td className="px-4 py-2 border">{data.id}</td>
+                    <td className="px-4 py-2 border">{data.title}</td>
                     <td className="px-4 py-2 border">{data.author}</td>
                     <td className="px-4 py-2 border">{data.numberOfPages}</td>
                     <td className="px-4 py-2 border">{data.publishedYear}</td>
+                    <td className="px-4 py-2 border">
+                      <button className="px-2 py-1 bg-yellow-500 text-white">
+                        Edit
+                      </button>
+                    </td>
                   </tr>
                 );
               })}
